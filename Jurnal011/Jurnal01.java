c4d24f99e922112a03438ba4db5775ff86e9fca3
Desi Jihan Fadhilah <de23jihan@gmail.com>
2023-09-29 14:41:50 +0700

    initial commit

--- Jurnal011/Jurnal01.java
+++ Jurnal011/Jurnal01.java
@@ -1,6 +1,6 @@
 public class Jurnal01 {
-    public static void main(String[] args){
-        String test = "test"
+    public static void main(String[] args) {
+        String test = "test";
         System.out.println("JA: " + test);
     }
 }
