3bfb8a0c8cfe8c8f9605b489602950e1bee6482a
Desi Jihan Fadhilah <de23jihan@gmail.com>
2023-09-29 14:46:30 +0700

    first commit

--- Jurnal011/Jurnal01.java
+++ Jurnal011/Jurnal01.java
@@ -1,6 +1,10 @@
 public class Jurnal01 {
     public static void main(String[] args) {
-        String test = "test";
-        System.out.println("JA: " + test);
+        String Nama = "Desi Jihan Fadhilah";
+        String NIM = "607062300071";
+        String Kelas = "D3IF 47-01";
+        System.out.println("Nama: " + Nama);
+        System.out.println("NIM: " + NIM);
+        System.out.println("Kelas: " + Kelas);
     }
 }
